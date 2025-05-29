46030d79d3d25b1d2cd545217296a8398e4b4ea4
Eram Manasia <eram.manasia@gmail.com>
2025-05-29 15:01:07 -0500

    trying to remove offer banner

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,6 @@
 const Navbar = () => {
+  const elementToRemove = document.getElementById('ofBar');
+  if(elementToRemove) elementToRemove.remove();
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
